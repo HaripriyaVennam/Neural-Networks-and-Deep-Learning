7a0a7f80f96f6b7856b27c56c1b236f750909751
HaripriyaVennam <hxv59520@ucmo.edu>
2025-04-15 15:16:45 -0500

    Delete research_paper directory

--- research_paper/modeling/__init__.py
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-
-from .baseline import Baseline
-
-def build_model(cfg, num_classes):
-    model = Baseline(num_classes, cfg.MODEL.LAST_STRIDE, cfg.MODEL.PRETRAIN_PATH, cfg.MODEL.NAME,
-                     cfg.MODEL.GENERALIZED_MEAN_POOL, cfg.MODEL.PRETRAIN_CHOICE)
-    return model
